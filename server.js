2ef484d1207469888b5fc500c493168834d79013
Your Name <you@example.com>
2024-10-03 15:04:38 +0100

    Teste de conexao no banco de dados e configuracaode variaveis de ambientes

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 import express from 'express'
 import Userouter from './src/routes/routes.js'
+import User from './src/models/user.js'
+import { Sequelize } from 'sequelize'
+import config from './src/config/database.js'
+
 const app=express()
+const sequelize=new Sequelize(config)
+User.init(sequelize)
 app.use(express.json())
 app.use('/user',Userouter)
 
-app.listen(4000,()=>console.log("server is running..."))
+
+sequelize.authenticate().
+then(()=>{
+    console.log("Conectado com banco de dados ")
+    app.listen(3000,()=> console.log('server is runing on port 3000')
+)
+}).catch((err)=>{
+    console.log('erro'+err)
+})
